7f5c04826e55a9e2fa5944fe924d1cddd64bcd1f
hakan özdemir <101719280+hakoez@users.noreply.github.com>
2025-03-24 18:34:37 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// React build klasörünü serve et
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-// Diğer tüm isteklerde index.html döndür (SPA routing için)
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-// Sunucu 8080 portunda çalışacak
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
