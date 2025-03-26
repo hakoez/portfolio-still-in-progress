df810e491e88c73bb798a502226404223a96f61b
hakan özdemir <101719280+hakoez@users.noreply.github.com>
2025-03-26 19:26:18 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/meinportfolio.duckdns.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/meinportfolio.duckdns.org/fullchain.pem')
+};
+
+// Statik React build
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server läuft auf http://localhost:${PORT}`);
+// HTTPS server
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS läuft auf https://meinportfolio.duckdns.org');
 });
+
+// HTTP → HTTPS 
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: 'https://' + req.headers.host + req.url });
+  res.end();
+}).listen(80);
